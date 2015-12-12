8a1e084b129a2aef03f5f291e9248027736f8ada
buckket <buckket@cock.li>
2015-12-12 03:10:22 +0100

    added new helper function: format_quotes()

--- helga/helper.py
+++ helga/helper.py
@@ -1,4 +1,7 @@
 import datetime
+import textwrap
+
+from functools import partial
 
 
 def format_timestamp(time):
@@ -10,15 +13,33 @@ def format_timestamp(time):
     return datetime.datetime.fromtimestamp(time).strftime('%d.%m.%Y %H:%M')
 
 
-def format_quote(quote):
+def format_quote(quote, short=False):
     """
-    Return a humand-readable string representation of a Quote object.
+    Return a human-readable string representation of a Quote object.
     :param quote: helga.quotes.Quote
+    :param short: bool
     :return: str
     """
-    return "Quote #{quote_id}: „<{author}> {text}“ (added by {added_by} at {date})".format(
-            quote_id=quote.quote_id,
-            author=quote.author.first_name,
-            text=quote.text,
-            added_by=quote.added_by.first_name,
-            date=format_timestamp(quote.date))
+    if short:
+            return "#{quote_id}: „<{author}> {text}“".format(
+                    quote_id=quote.quote_id,
+                    author=quote.author.first_name,
+                    text=textwrap.shorten(quote.text, 50))
+    else:
+            return "Quote #{quote_id}: „<{author}> {text}“ (added by {added_by} at {date})".format(
+                    quote_id=quote.quote_id,
+                    author=quote.author.first_name,
+                    text=quote.text.replace('\n', ' '),
+                    added_by=quote.added_by.first_name,
+                    date=format_timestamp(quote.date))
+
+
+def format_quotes(quotes):
+    """
+    Return a human-readable string representation of multiple Quote objects.
+    :param quotes: iterable of helga.quotes.Quote
+    :return: str
+    """
+    format_quote_short = partial(format_quote, short=True)
+    return ', '.join(map(format_quote_short, quotes))
+
