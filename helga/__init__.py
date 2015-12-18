880c27e6952c15de5a0a65486ab8b8260642499b
buckket <buckket@cock.li>
2015-12-18 16:05:09 +0100

    revamped README and __init__.py

---
+++ helga/__init__.py
@@ -0,0 +1,12 @@
+"""
+    (t)helga
+    ~~~~~~~~
+
+    A Telegram bot in Python 3.
+
+    :copyright: (c) 2015 by buckket.
+    :license: MIT, see LICENSE for more details.
+"""
+
+
+__version__ = '0.1-dev'
