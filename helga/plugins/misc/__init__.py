8085a43e65e0903798963ee6f1a5fd16db0d38b6
teddydestodes <derkomtur@schattengang.net>
2015-12-24 23:01:58 +0100

    added dice command

--- helga/plugins/misc/__init__.py
+++ helga/plugins/misc/__init__.py
@@ -9,6 +9,7 @@ class MiscPlugin(Plugin):
         self.bot.register_command('yn', self.yn)
         self.bot.register_command('random', self.random)
         self.bot.register_command('test', self.test)
+        self.bot.register_command('dice', self.dice)
 
     def yn(self, msg, *args):
         """ Prints 'yes' or 'no' """
@@ -20,3 +21,19 @@ class MiscPlugin(Plugin):
 
     def test(self, msg, *args):
         self.bot.send_photo(msg.chat.id, photo=open('test.jpg', 'rb'))
+
+    def dice(self, msg, *args):
+        """rolls a dice, format <rolls>d<sides>"""
+        if (len(args) == 0):
+            args = ['1d6']
+        try:
+            rolls, maxint = args[0].split('d')
+            rolls = int(rolls)
+            maxint = int(maxint)
+            results = []
+            for roll in range(rolls):
+                results.append(str(random.randint(1, maxint+1)))
+            self.bot.send_reply(msg, ' '.join(results))
+        except Exception as e:
+            self.bot.send_reply(msg, 'Please use Format <numrolls>d<sides>')
+
