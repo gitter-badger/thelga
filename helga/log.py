7be216a9e3c42422e97bd82d988b2e388fbf490c
buckket <buckket@cock.li>
2016-01-04 16:54:32 +0100

    updated formatting in log.py

--- helga/log.py
+++ helga/log.py
@@ -1,21 +1,23 @@
+import os
 import logging
 import logging.handlers
-import os
 
 from helga import config
 
 
 def init_logging():
     logger = logging.getLogger()
+
     formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+
     fh = logging.handlers.TimedRotatingFileHandler(os.path.join(config.workdir, 'helga.log'), when='midnight')
     fh.setLevel(logging.INFO)
     fh.setFormatter(formatter)
     logger.addHandler(fh)
+
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
     ch.setFormatter(formatter)
     logger.addHandler(ch)
+
     logger.setLevel(logging.DEBUG)
-
-
